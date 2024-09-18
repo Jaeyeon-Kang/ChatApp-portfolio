7b57bdbcba613f60f9e108ba397ced3d838bfa1b
Jaeyeon-Kang <wodus770@gmail.com>
2024-09-19 00:11:06 +0900

    [setting: electron.js 하위 폴더로 frontend 폴더 사용, electron.js 설치 및  main.js와 preload.js 초기 설정

---
+++ chatapp-backend/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      webSecurity: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:3000");
+}
+
+app.disableHardwareAcceleration();
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
